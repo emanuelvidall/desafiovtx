bdf32a73b9eeda87863d7e4830248886d680d7ae
emanuelvidall <emanuelvidalrm@gmail.com>
2023-03-15 20:25:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, 400, 400);
+
+ctx.fillStyle = 'white';
+
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 === 0) {
+        ctx.fillRect(i * 50, j * 50, 50, 50);
+        }
+    }
+}
+
+function getEntrada(){
+    const codigos = ['Vazio', 'Peão', 'Bispo', 'Cavalo', 'Torre', 'Rei', 'Rainha'];
+    //remocao de todo e qualquer espaco em branco, incluindo empty strings no caso de enter na entrada de dados. Usando .replace para remover os enters e .split para separar os elementos em um array e usando .filter para remover os elementos vazios combinado com o .trim para remover os espacos em branco incluindo empty strings.
+    const arrayEntrada = document.getElementById('entrada').value.split("").join(" ").replace( /\n/g, " ").split(' ').filter(e => String(e).trim());
+    console.log(arrayEntrada);
+    const formulario = document.getElementById('formulario');
+    const divResultado = document.getElementById('resultadoM');
+    divResultado.style.opacity = 1;
+    divResultado.innerHTML = "";
+    divResultado.style.translate = "0px -200px";
+
+    for (let i = 0; i < 7; i++){
+        function checkPeca(peca){
+            return peca == i;
+        }
+        let resultado = arrayEntrada.filter(checkPeca);
+        divResultado.innerHTML += "<strong>" + codigos[i] + "</strong>" + ": " + resultado.length + " peça(s) " + "<br>";
+        console.log(codigos[i] + " " + resultado.length + " peça(s) ");
+        console.log(resultado.filter(checkPeca));
+    }
+
+    formulario.reset();
+}
